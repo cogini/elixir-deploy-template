5dfcd40e99f38b056343adc7d91e2dad6a594ff4
Jake Morrison <jake@cogini.com>
2018-05-05 11:14:59 +0800

    Add shutdown_flag

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule DeployTemplate.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:shutdown_flag, github: "cogini/shutdown_flag"},
     ]
   end
 end
