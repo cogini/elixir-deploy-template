8569569d995d24865b93e0c1cbd9c543911f0114
Jake Morrison <jake@cogini.com>
2018-05-20 14:10:01 +0000

    Put cookie back

--- rel/config.exs
+++ rel/config.exs
@@ -37,7 +37,8 @@ end
 environment :prod do
   set include_erts: true
   set include_src: false
-  # set cookie: File.read!("config/cookie.txt") |> String.to_atom
+  set cookie: File.read!("config/cookie.txt") |> String.to_atom
+  # set cookie: :prod
   set vm_args: "rel/vm.args.eex"
 end
 
